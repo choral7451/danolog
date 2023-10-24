cada71fdd6023cf9a4ed6659376527991812d29b
daniel <daniel@twosun.com>
2023-10-24 16:20:26 +0900

    Upt. rest docs custom updated

--- src/test/java/com/danolog/api/controller/PostControllerDocTest.java
+++ src/test/java/com/danolog/api/controller/PostControllerDocTest.java
@@ -2,44 +2,45 @@ package com.danolog.api.controller;
 
 import com.danolog.api.domain.Post;
 import com.danolog.api.repository.PostRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.danolog.api.request.PostCreate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.danolog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+  @Autowired
   private MockMvc mockMvc;
 
   @Autowired
   private PostRepository postRepository;
 
-  @BeforeEach
-  void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-      .apply(documentationConfiguration(restDocumentation))
-      .build();
-  }
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Test
-  @DisplayName("글 단건 조회 테스트")
+  @DisplayName("글 단건 조회")
   void test1() throws Exception {
     Post post = Post.builder()
       .title("제목")
@@ -51,6 +52,39 @@ public class PostControllerDocTest {
     this.mockMvc.perform(get("/posts/{postId}",post.getId())
         .contentType(APPLICATION_JSON))
       .andExpect(status().isOk())
-      .andDo(document("index"));
+      .andDo(document("post-inquiry",
+        pathParameters(
+          parameterWithName("postId").description("게시글 ID")
+        ),
+        responseFields(
+          fieldWithPath("id").description("게시글 ID"),
+          fieldWithPath("title").description("게시글 제목"),
+          fieldWithPath("content").description("게시글 내용")
+        )
+      ));
+  }
+
+  @Test
+  @DisplayName("글 등록")
+  void test2() throws Exception {
+    PostCreate request = PostCreate.builder()
+      .title("나는 다니엘입니다.")
+      .content("내용입니다.")
+      .build();
+
+    String json = objectMapper.writeValueAsString(request);
+
+    mockMvc.perform(post("/posts")
+        .contentType(APPLICATION_JSON)
+        .accept(APPLICATION_JSON)
+        .content(json))
+      .andExpect(status().isOk())
+      .andDo(document("post-create",
+        requestFields(
+          fieldWithPath("title").description("제목")
+            .attributes(key("constraint").value("좋은제목 입력해주세요.")),
+          fieldWithPath("content").description("내용").optional()
+        )
+      ));
   }
 }
