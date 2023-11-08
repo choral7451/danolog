5c5af4fea60412f89c7f3bf77786475bda529af4
daniel <chorales@naver.com>
2023-11-09 01:47:36 +0900

    Upt. login logic updated with jwt

--- src/main/java/com/danolog/api/controller/AuthController.java
+++ src/main/java/com/danolog/api/controller/AuthController.java
@@ -3,17 +3,17 @@ package com.danolog.api.controller;
 import com.danolog.api.request.Login;
 import com.danolog.api.response.SessionResponse;
 import com.danolog.api.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 @Slf4j
 @RestController
@@ -21,21 +21,15 @@ import java.time.Duration;
 public class AuthController {
 
   private final AuthService authService;
-
+  private final String KEY = "1oaAWvrgDcYmUZNqnyhJRQHDfWGkVK3Vnt4J0mn7vy0=";
   @PostMapping("/auth/login")
-  public ResponseEntity<Object> login(@RequestBody @Valid Login login) {
-    String accessToken = authService.signin(login);
-    ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
-      .domain("localhost") // todo 서버 환경에 따른 분리 필요
-      .path("/")
-      .httpOnly(true)
-      .secure(false)
-      .maxAge(Duration.ofDays(30))
-      .sameSite("Strict")
-      .build();
+  public SessionResponse login(@RequestBody @Valid Login login) {
+    Long userId = authService.signin(login);
+    log.info(">>>>>>>", userId);
+    SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(KEY));
 
-    return ResponseEntity.ok()
-      .header(HttpHeaders.SET_COOKIE, cookie.toString())
-      .build();
+    String jws = Jwts.builder().subject(String.valueOf(userId)).signWith(key).compact();
+    log.info(">>>>>>>", jws);
+    return new SessionResponse(jws);
   }
 }
