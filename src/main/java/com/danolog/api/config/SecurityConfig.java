3fd777ce8d1647f51b246140ed7f63fee05b793e
daniel <chorales@naver.com>
2023-11-12 01:00:23 +0900

    Upt. admin user for server developer set

--- src/main/java/com/danolog/api/config/SecurityConfig.java
+++ src/main/java/com/danolog/api/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,7 +33,32 @@ public class SecurityConfig {
         .requestMatchers("/auth/login").permitAll()
         .anyRequest().authenticated()
       )
+      .formLogin(form -> form
+        .loginPage("/auth/login")
+        .loginProcessingUrl("/auth/login")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/")
+      )
+      .userDetailsService(userDetailsService())
       .csrf(AbstractHttpConfigurer::disable) // 확인 필요
       .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    UserDetails user = User
+      .withUsername("danolman")
+      .password("1234")
+      .roles("ADMIN")
+      .build();
+    manager.createUser(user);
+    return manager;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
   }
 }
