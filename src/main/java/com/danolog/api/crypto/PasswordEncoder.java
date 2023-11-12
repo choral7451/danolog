fbb5286d9c2992a7eea4a4346cf97f9685a76f1b
daniel <chorales@naver.com>
2023-11-13 00:30:52 +0900

    Upt. login, signup api updated with security

--- src/main/java/com/danolog/api/crypto/PasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.danolog.api.crypto;
-
-public interface PasswordEncoder {
-  String encrypt(String rawPassword);
-
-  boolean matches(String rawPassword, String encryptedPassword);
-}
--- src/main/java/com/danolog/api/service/AuthService.java
+++ src/main/java/com/danolog/api/service/AuthService.java
@@ -1,11 +1,11 @@
 package com.danolog.api.service;
 
-import com.danolog.api.crypto.PasswordEncoder;
 import com.danolog.api.domain.User;
 import com.danolog.api.exception.AlreadyExistsEmailException;
 import com.danolog.api.repository.UserRepository;
 import com.danolog.api.request.Signup;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,14 +16,13 @@ public class AuthService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
-
   public void signup(Signup signup) {
     Optional<User> userOptional = userRepository.findByEmail(signup.getEmail());
     if (userOptional.isPresent()) {
       throw new AlreadyExistsEmailException();
     }
 
-    String encryptedPassword = passwordEncoder.encrypt(signup.getPassword());
+    String encryptedPassword = passwordEncoder.encode(signup.getPassword());
 
     var user = User.builder()
       .name(signup.getName())
