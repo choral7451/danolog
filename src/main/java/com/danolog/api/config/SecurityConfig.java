5ba5cd75e18402ec1e632e96f55f9bb8491c9a11
daniel <chorales@naver.com>
2023-11-13 00:46:13 +0900

    Upt. give user authority and role

--- src/main/java/com/danolog/api/config/SecurityConfig.java
+++ src/main/java/com/danolog/api/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.danolog.api.domain.User;
 import com.danolog.api.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -15,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -31,8 +31,10 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
       .authorizeHttpRequests(request -> request
-        .requestMatchers(HttpMethod.POST,"/auth/signup").permitAll()
-        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+        .requestMatchers("/auth/signup").permitAll()
+        .requestMatchers("/auth/login").permitAll()
+        .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+        .requestMatchers("/admin").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') AND hasAuthority('WRITE')"))
         .anyRequest().authenticated()
       )
       .formLogin(form -> form
